d41764c627eaec4a5c1303b86a436b18574a4d38
unknown <dustin.guy.jackson@gmail.com>
2020-03-10 10:58:50 -0700

    layed out file structure, boilerplate connection.js and server.js, main.handlebars

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+			
+//setting handlebars as template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//middleware launching - handles data parsing with Express
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+const routes = require('./controllers/burgerController');
+
+app.use(routes);
+
+
+app.listen(PORT, function() {
+	// Log (server-side) when our server has started
+	console.log('Server listening on: http: //localhost:' + PORT);
+});
