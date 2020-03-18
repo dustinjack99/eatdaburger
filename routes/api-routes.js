b0ba3eede8fc0212b99ce838f38014b5eb060428
unknown <dustin.guy.jackson@gmail.com>
2020-03-18 12:09:44 -0700

    cant get update or delete to work and I have no idea why

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,49 +1,48 @@
-
-var db = require("../models");
+var db = require('../models');
 
 module.exports = function(app) {
-
-  app.get('/', function (req, res) {
+  app.get('/', function(req, res) {
     db.Burger.findAll({}).then(function(burgers) {
       res.render('burgers', {
         burgers
-      })
+      });
     });
-});
+  });
 
-  app.post("/", function(req, res) {
+  app.post('/api/burgers', function(req, res) {
     db.Burger.create({
       burger_name: req.body.burgerName,
       devoured: 0
     }).then(function(dbBurger) {
+      // res.json(dbBurger);
       res.redirect('/');
     });
   });
- 
-  app.delete("/delete/:id", function(req, res) {
-    console.log(req)
+
+  app.delete('/api/burgers/delete/:id', function(req, res) {
     db.Burger.destroy({
       where: {
         id: req.params.id
       }
     }).then(function(dbBurger) {
-      console.log(res)
-      res.json(dbBurger)
+      // res.json(dbBurger);
       res.redirect('/');
     });
-
   });
 
-  app.put("/put", function(req, res) {
-    db.Burger.update({
-      burger_name: req.body.burger_name,
-      devoured: 1
-    }, {
-      where: {
-        id: req.body.id
+  app.put('/api/burgers/eat', function(req, res) {
+    db.Burger.update(
+      {
+        burger_name: req.body.burger_name,
+        devoured: 1
+      },
+      {
+        where: {
+          id: req.body.id
+        }
       }
-    }).then(function(dbBurger) {
-      res.json(dbBurger)
+    ).then(function(dbBurger) {
+      // res.json(dbBurger);
       res.redirect('/');
     });
   });
--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-const exphbs = require("express-handlebars");
+var express = require('express');
+const exphbs = require('express-handlebars');
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
+var db = require('./models');
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 // Use handlebars view engine
 app.set('view engine', 'handlebars');
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-require("./routes/api-routes.js")(app);
+require('./routes/api-routes.js')(app);
 
-db.sequelize.sync({extended: true}).then(function() {
+db.sequelize.sync({ extended: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log('App listening on PORT ' + PORT);
   });
 });
